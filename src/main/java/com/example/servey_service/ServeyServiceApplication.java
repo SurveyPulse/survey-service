e253ccdfa1e6915bbdcd7c9b92e11041b34d5b1f
201912025 <michi2012@naver.com>
2025-04-03 12:28:20 +0900

    fix: 컴포넌트 스캔오류및 BaseEntity 적용 오류 해결

--- src/main/java/com/example/servey_service/ServeyServiceApplication.java
+++ src/main/java/com/example/servey_service/ServeyServiceApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableDiscoveryClient
 @EnableFeignClients
+@ComponentScan(basePackages = {"com.example.servey_service", "com.example.global"})
+@EnableJpaAuditing
 public class ServeyServiceApplication {
 
 	public static void main(String[] args) {
