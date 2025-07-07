50fd024b78cf15f11ebc974454ccc9230d4c325b
201912025 <michi2012@naver.com>
2025-07-07 14:05:12 +0900

    feat: 서킷브레이커 retry, timelimiter 추가

--- src/main/java/com/example/servey_service/client/service/UserClientService.java
+++ src/main/java/com/example/servey_service/client/service/UserClientService.java
@@ -3,6 +3,8 @@ package com.example.servey_service.client.service;
 import com.example.servey_service.client.UserClient;
 import com.example.servey_service.dto.response.RespondentUserDto;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,8 @@ public class UserClientService {
     private final UserClient userClient;
 
     @CircuitBreaker(name = "userService", fallbackMethod = "fallbackGetUserDto")
+    @Retry(name = "userService", fallbackMethod = "fallbackGetUserDto")
+    @TimeLimiter(name = "userService")
     public RespondentUserDto getUserDto(Long userId) {
         return userClient.getUserDto(userId);
     }
