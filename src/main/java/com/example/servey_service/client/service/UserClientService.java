af7a06c3a88e9e304057d1f3e6fd58dcd01074f8
201912025 <michi2012@naver.com>
2026-02-01 14:07:47 +0900

    refactor: 설정 값 외부화, 유레카 비활성화 및 Resilience4j 튜닝

--- src/main/java/com/example/servey_service/client/service/UserClientService.java
+++ src/main/java/com/example/servey_service/client/service/UserClientService.java
@@ -4,7 +4,6 @@ import com.example.servey_service.client.UserClient;
 import com.example.servey_service.dto.response.RespondentUserDto;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,7 +17,6 @@ public class UserClientService {
 
     @CircuitBreaker(name = "userService", fallbackMethod = "fallbackGetUserDto")
     @Retry(name = "userService", fallbackMethod = "fallbackGetUserDto")
-    @TimeLimiter(name = "userService")
     public RespondentUserDto getUserDto(Long userId) {
         return userClient.getUserDto(userId);
     }
